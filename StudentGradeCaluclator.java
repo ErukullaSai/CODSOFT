53662e644b60c64be061caa2b580b233a0e05ea1
ErukullaSai <23211a6646@bvrit.ac.in>
2024-06-08 20:41:47 +0530

    Add files via upload

---
+++ StudentGradeCaluclator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class StudentGradeCaluclator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = sc.nextInt();
+
+        int totalMarks = 0;
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + i + ": ");
+            int marks = sc.nextInt();
+            totalMarks += marks;
+        }
+
+        double averagePercentage = (double) totalMarks / numSubjects;
+
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+
+        // Assign grades
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        System.out.println("Grade: " + grade);
+
+        sc.close();
+    }
+}
